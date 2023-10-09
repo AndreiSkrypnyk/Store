0d47b8f0504ea461362aad39dd54122d848c8f03
AndreiSkrypnyk <andrii.skrypnyk.it@gmail.com>
2023-10-09 23:55:22 +0300

    Create a database context class
    Define properties of type DbSet<> for each entity.

---
+++ BookStore.Infrastructure/BookStoreCodeFirstDbContext.cs
@@ -0,0 +1,29 @@
+﻿using BookStore.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Infrastructure
+{
+    public class BookStoreCodeFirstDbContext : DbContext
+    {
+        public BookStoreCodeFirstDbContext(DbContextOptions<BookStoreCodeFirstDbContext> options) : base(options) { }
+
+        public BookStoreCodeFirstDbContext() { }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BookStoreCodeFirstDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
