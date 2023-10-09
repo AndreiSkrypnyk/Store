e0ec0eb6525590b9aa994f0927de69fa033b15be
AndreiSkrypnyk <andrii.skrypnyk.it@gmail.com>
2023-10-09 22:24:41 +0300

    Сreate entities
    Created 5 entities:
    
    1. Book
    2. Order
    3. OrderItem
    4. User
    5. UserRole

---
+++ BookStore.Core/Entities/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Core.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Genre { get; set; }
+        public string Image { get; set; }
+    }
+}
