ddd23c2a4e867f279fadc9737a4b1e17e97a371a
AndreiSkrypnyk <andrii.skrypnyk.it@gmail.com>
2024-01-27 21:54:59 +0200

    * Make ShoppingCartVM more Dynamic

--- BookStore.Core/ViewModels/ShoppingCartVM.cs
+++ BookStore.Core/ViewModels/ShoppingCartVM.cs
@@ -10,7 +10,7 @@ namespace BookStore.Core.ViewModels
     public class ShoppingCartVM
     {
         public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
-        public double OrderTotal { get; set; }
+        public OrderHeader OrderHeader { get; set; }
 
     }
 }
