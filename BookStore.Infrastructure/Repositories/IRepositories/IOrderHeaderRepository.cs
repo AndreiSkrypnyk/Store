e51131d574eb3fc0f793b7552c4d607f1c8652dc
AndreiSkrypnyk <andrii.skrypnyk.it@gmail.com>
2024-01-27 21:28:10 +0200

    * Add Order Header and Detail Repository

---
+++ BookStore.Infrastructure/Repositories/IRepositories/IOrderHeaderRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookStore.Core.Entities;
+
+namespace BookStore.Infrastructure.Repositories.IRepositories
+{
+    public interface IOrderHeaderRepository : IRepository<OrderHeader>
+    {
+        void Update(OrderHeader obj);
+    }
+}
---
+++ BookStore.Infrastructure/Repositories/OrderHeaderRepository.cs
@@ -0,0 +1,20 @@
+﻿using BookStore.Core.Entities;
+using BookStore.Infrastructure.Data;
+using BookStore.Infrastructure.Repositories.IRepositories;
+
+namespace BookStore.Infrastructure.Repositories
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private BookStoreCodeFirstDbContext _db;
+        public OrderHeaderRepository(BookStoreCodeFirstDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(OrderHeader obj)
+        {
+            _db.OrderHeaders.Update(obj);
+        }
+    }
+}
