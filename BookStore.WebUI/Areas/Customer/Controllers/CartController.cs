e43f197f2dcca2b6cc236548c728f78bc223a45c
AndreiSkrypnyk <andrii.skrypnyk.it@gmail.com>
2024-02-03 21:42:35 +0200

    * Stripe in Action

--- BookStore.WebUI/Areas/Customer/Controllers/CartController.cs
+++ BookStore.WebUI/Areas/Customer/Controllers/CartController.cs
@@ -4,6 +4,7 @@ using BookStore.Infrastructure.Repositories.IRepositories;
 using BookStore.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Stripe.Checkout;
 using System.Security.Claims;
 
 namespace BookStore.Areas.Customer.Controllers
@@ -126,7 +127,39 @@ namespace BookStore.Areas.Customer.Controllers
 				{
 					//it is a regular customer account and we need to capture payment
 					//stripe logic
-					
+					var domain = "https://localhost:7191/";
+					var options = new SessionCreateOptions
+					{
+						SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
+						CancelUrl = domain + "customer/cart/index",
+						LineItems = new List<SessionLineItemOptions>(),
+						Mode = "payment",
+					};
+
+					foreach (var item in ShoppingCartVM.ShoppingCartList)
+					{
+						var sessionLineItem = new SessionLineItemOptions
+						{
+							PriceData = new SessionLineItemPriceDataOptions
+							{
+								UnitAmount = (long)(item.Price * 100), // $20.50 => 2050
+								Currency = "usd",
+								ProductData = new SessionLineItemPriceDataProductDataOptions
+								{
+									Name = item.Product.Title
+								}
+							},
+							Quantity = item.Count
+						};
+						options.LineItems.Add(sessionLineItem);
+					}
+
+					var service = new SessionService();
+					Session session = service.Create(options);
+					_unitOfWork.OrderHeader.UpateStripePaymentID(ShoppingCartVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
+					_unitOfWork.Save();
+					Response.Headers.Add("Location", session.Url);
+					return new StatusCodeResult(303);
 				}
 
 				return RedirectToAction(nameof(OrderConfirmation), new { id = ShoppingCartVM.OrderHeader.Id });
