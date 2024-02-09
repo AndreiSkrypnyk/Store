3008ae2bbf28569aecd02499623d307d46f5abb0
AndreiSkrypnyk <andrii.skrypnyk.it@gmail.com>
2024-02-09 20:14:05 +0200

    * OrderVM and Order Controller

---
+++ BookStore.WebUI/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using BookStore.Core.Entities;
+using BookStore.Infrastructure.Repositories.IRepositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.WebUI.Areas.Admin.Controllers
+{
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+		{
+			return View();
+		}
+
+
+		#region API CALLS
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			return Json(new { data = objOrderHeaders });
+		}
+
+		#endregion
+	}
+}
