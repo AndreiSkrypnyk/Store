c0d8b7f7cbe7842f8f1a453a2d5d124677927d33
AndreiSkrypnyk <andrii.skrypnyk.it@gmail.com>
2024-02-14 21:07:27 +0200

    * Creating Admin and Employee Accounts

--- BookStore.WebUI/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BookStore.WebUI/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -213,7 +213,15 @@ namespace BookStore.WebUI.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(SD.Role_Admin))
+                        {
+                            TempData["success"] = "New User Created Successfully";
+                        }
+                        else
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                        }
+                        
                         return LocalRedirect(returnUrl);
                     }
                 }
