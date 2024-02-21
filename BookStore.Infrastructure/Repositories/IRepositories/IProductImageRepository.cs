ff5568d39b06b00865da854765fecb470ed23733
AndreiSkrypnyk <andrii.skrypnyk.it@gmail.com>
2024-02-21 15:36:14 +0200

    * Add Product Image Repository

---
+++ BookStore.Infrastructure/Repositories/IRepositories/IProductImageRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookStore.Core.Entities;
+
+namespace BookStore.Infrastructure.Repositories.IRepositories
+{
+    public interface IProductImageRepository : IRepository<ProductImage>
+    {
+        void Update(ProductImage obj);
+    }
+}
---
+++ BookStore.Infrastructure/Repositories/ProductImageRepository.cs
@@ -0,0 +1,20 @@
+﻿using BookStore.Core.Entities;
+using BookStore.Infrastructure.Data;
+using BookStore.Infrastructure.Repositories.IRepositories;
+
+namespace BookStore.Infrastructure.Repositories
+{
+    public class ProductImageRepository : Repository<ProductImage>, IProductImageRepository
+    {
+        private BookStoreCodeFirstDbContext _db;
+        public ProductImageRepository(BookStoreCodeFirstDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(ProductImage obj)
+        {
+            _db.ProductImages.Update(obj);
+        }
+    }
+}
