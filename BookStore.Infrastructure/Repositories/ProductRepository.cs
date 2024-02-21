5081a843ca7f17486e4894d90c09ba7579b0eb16
AndreiSkrypnyk <andrii.skrypnyk.it@gmail.com>
2024-02-21 20:10:42 +0200

    * Upload Images on Product Upsert * Display Image on Update Product Page * Delete Image logic

--- BookStore.Infrastructure/Repositories/ProductRepository.cs
+++ BookStore.Infrastructure/Repositories/ProductRepository.cs
@@ -26,6 +26,7 @@ namespace BookStore.Infrastructure.Repositories
                 objFromDb.Description = obj.Description;
                 objFromDb.CategoryId = obj.CategoryId;
                 objFromDb.Author = obj.Author;
+                objFromDb.ProductImages = obj.ProductImages;
                 //if (obj.ImageUrl != null)
                 //{
                 //    objFromDb.ImageUrl = obj.ImageUrl;
