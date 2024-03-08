33838c8336b63ffa937c84786f39692715d84937
AndreiSkrypnyk <andrii.skrypnyk.it@gmail.com>
2024-03-08 20:54:51 +0200

    fix trash code

--- BookStore.Infrastructure/Data/BookStoreCodeFirstDbContext.cs
+++ BookStore.Infrastructure/Data/BookStoreCodeFirstDbContext.cs
@@ -10,10 +10,6 @@ namespace BookStore.Infrastructure.Data
         public BookStoreCodeFirstDbContext(DbContextOptions<BookStoreCodeFirstDbContext> options) : base(options) { }
 
         public BookStoreCodeFirstDbContext() { }
-        public DbSet<Book> Books { get; set; }
-        public DbSet<Order> Orders { get; set; }
-        public DbSet<OrderItem> OrderItems { get; set; }
-        public DbSet<User> Users { get; set; }
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
